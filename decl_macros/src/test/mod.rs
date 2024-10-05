70b8dd311005e575861a1f86d1643db6040c2e31
Florian Meißner <936176+t-nil@users.noreply.github.com>
2024-10-05 15:00:12 +0200

    feat(decl_macros/test/test_non_utf8_path): a method to test parsing of non-UTF8 paths (currently only unix)

--- decl_macros/src/test/mod.rs
+++ decl_macros/src/test/mod.rs
@@ -1,4 +1,6 @@
 pub mod insta {
+    use std::path::PathBuf;
+
     /// https://insta.rs/docs/cmd/#filtering
     ///
     /// Filters common, changing expressions from test snapshots, like tempfiles.
@@ -17,6 +19,23 @@ pub mod insta {
             // Convert windows paths to Unix Paths.
             settings.add_filter(r"\\\\?([\w\d.])", "/$1");
             let _bound = settings.bind_to_scope();
+        }
+    }
+
+    // Creating non-utf8 path is an OS-specific pain, so let's run this only on
+    // linux, where arbitrary bytes work.
+    //
+    // (stolen from https://github.com/rust-lang/cargo/pull/9226/files#diff-9977238c61100eb9f319febd88e2434b304ac401f0da3b50d00d7c91de319e2fR2957-R2966)
+    pub fn generate_non_utf8_path<T>() -> PathBuf {
+        #[cfg(target_os = "linux")]
+        {
+            use std::ffi::OsString;
+            use std::os::unix::ffi::OsStringExt;
+            use std::path::PathBuf;
+
+            let wrong = PathBuf::from(OsString::from_vec(vec![255]));
+
+            wrong
         }
     }
 }
